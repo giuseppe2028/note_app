69d624669ddfba2aba62270ad16c84c948ab7c09
Giuseppe Barone <giuseppebarone2028@gmail.com>
2023-05-14 21:55:45 +0200

    Aggiunto colori che cambiano con il tema

--- lib/SecondPage.dart
+++ lib/SecondPage.dart
@@ -12,11 +12,17 @@ class _SecondPageState extends State<SecondPage> {
   // ignore_for_file: prefer_const_constructors
   @override
   Widget build(BuildContext context) {
+    Color coloreTesto = Colors.white;
     int _maxLine;
     if (MediaQuery.of(context).orientation == Orientation.landscape) {
       _maxLine = 3;
     } else {
       _maxLine = 25;
+    }
+    if (back == Colors.black87) {
+      coloreTesto = Colors.white;
+    } else {
+      coloreTesto = Colors.black87;
     }
 
     print("entrato");
@@ -38,9 +44,10 @@ class _SecondPageState extends State<SecondPage> {
                 ),
                 TextField(
                   decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Enter a Title',
-                  ),
+                      border: InputBorder.none,
+                      hintText: 'Enter a Title',
+                      hintStyle: TextStyle(color: coloreTesto)),
+                  style: TextStyle(color: coloreTesto),
                 ),
                 TextField(
                   keyboardType: TextInputType.multiline,
@@ -48,7 +55,9 @@ class _SecondPageState extends State<SecondPage> {
                   decoration: InputDecoration(
                     border: InputBorder.none,
                     hintText: 'Enter your text here',
+                    hintStyle: TextStyle(color: coloreTesto),
                   ),
+                  style: TextStyle(color: coloreTesto),
                 ),
                 Container(
                   alignment: Alignment.bottomRight,
